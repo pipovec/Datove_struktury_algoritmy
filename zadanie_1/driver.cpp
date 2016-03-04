2829e11b2356812c042575c72800a8f794d24b3f
pipovec <pipovec@gmail.com>
2016-03-04 19:48:00 +0100

    modified:   driver.cpp 	modified:   game.txt 	driver

--- zadanie_1/driver.cpp
+++ zadanie_1/driver.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
+#include <fstream>
 
-using namespace std;
+/* Trieda na pracu s volbou New */
+class ChoseNew{
+	
+	
+	public:
+		std::ofstream file;
+
+		int SetNewData()
+		{
+			OpenFile();
+			
+			CloseFile();
+			return 0;
+		}
+
+	private:
+		
+		void OpenFile()
+		{
+			file.open("game.txt", std::ofstream::trunc);
+			std::cout << "Som tu." << std::endl;
+		}	
+
+		void CloseFile()
+		{
+			file.close();
+		}
+
+};
+
+/* Trieda na pracu s volbou Load */
+class ChoseLoad{
+	public:
+
+	private:	
+};
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    
+    using namespace std;
+   	ChoseNew New;
+
+    New.SetNewData();
+
+
+    
     return 0;
 }
+
+
+
+
+
